5f76d88b853aa5bd96d6bb339a7979f2b4d68882
David Schnurr <schnurr@gmail.com>
2020-09-24 20:30:19 -0700

    temp

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  })
+  var body = JSON.stringify({"prompt": "hello world", "max_tokens": 9 });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.THE_KEY}`
+    },
+    body: body,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.openai.com/v1/engines/davinci/completions", requestOptions)
+    .then(response => response.json())
+    .then(json => {
+      res.status(200).json(json);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('An error occurred');
+    })
+});
 };
